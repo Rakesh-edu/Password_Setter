79aa7af2c5f2356dc138b1d9e10393360680020d
Rakesh-edu <rakeshbanavath5@gmail.com>
2024-09-23 17:54:33 +0530

    This is a project based on show and hide password

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [password,setPassword]=useState(false);
+  const getPassword=()=>{
+    setPassword(!password);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div className="body">
+    <div className="input-div">
+      <input type={password?"text":"password"} className='input-box' />
+      <button onClick={getPassword}>{(password===true)?"HIDE":"SHOW"}</button>
+      </div>
     </div>
+   </>
   );
 }
 
